0db379a11079bf85ec6ac487e348e51ccfbc4b75
Singhal-Anubhav <60470022+Singhal-Anubhav@users.noreply.github.com>
2020-05-29 20:56:34 +0530

    Add files via upload

---
+++ Intern_project/frontend/src/Components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = (props) => {
+    return(
+        <div>
+            <h1 className = "title-container">Get Team Details</h1>
+        </div>
+    )
+}
+
+export default Title;
